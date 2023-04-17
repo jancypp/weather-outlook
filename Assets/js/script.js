88d0a16c2a1206c5a2830881ce979afc7b4c84d5
jancypp <120283327+jancypp@users.noreply.github.com>
2023-04-16 18:28:37 -0600

    rendering 5day forecast on the page

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,13 +1,16 @@
 //psuedocode
 // I want to open a webpage and input the location so that I can see the weather forecast
 //-form iput for user and event listener for submit
-var submitButton = document.querySelector('.btn');
+var submitButton = document.querySelector('.searchBtn');
 submitButton.addEventListener("click", function (event) {
     event.preventDefault()
-    var citySearch = document.querySelector("#city-search");
-    fetchAPI(citySearch.value)
+    var citySearch = document.querySelector("#city-search").value;
+    fetchAPI(citySearch)
 });
 
+var weatherDetails = document.querySelectorAll(".card-text");
+var weatherTitle = document.querySelectorAll(".card-title");
+var weatherIcon = document.querySelectorAll(".weather-icon");
 //I want current weather for that loction to display
 //-fetch/response/data weather API
 //-data fetched should include current weather, icon, temperature, humidity, and wind speed
@@ -31,12 +34,28 @@ var fetchAPI = function (cityName) {
         })
         .then(function (data) {
             console.log(data);
+        weatherTitle[0].textContent = "City :"+data.name
+           weatherDetails[0].textContent = "Temp:" + data.main.temp
+           weatherDetails[1].textContent = "Wind:" + data.wind.speed
+           weatherDetails[2].textContent = "Humidity:" + data.main.humidity
+           weatherIcon[0].setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
             fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey  + "&units=imperial")
                 .then(function (response) {
                     return response.json()
                 })
                 .then(function(fiveDay){
                     console.log(fiveDay);
+                    let j = 3;
+                    let z=1;
+                    for (let i = 0; i<fiveDay.list.length; i = i+8){
+                        weatherTitle[z].textContent = fiveDay.list[i].dt_txt
+                        weatherDetails[j].textContent = "Temp:" + fiveDay.list[i].main.temp
+                        weatherDetails[j+1].textContent = "Wind:" + fiveDay.list[i].wind.speed
+                        weatherDetails[j+2].textContent = "Humidity:" + fiveDay.list[i].main.humidity
+                        weatherIcon[z].setAttribute("src", `https://openweathermap.org/img/wn/${fiveDay.list[i].weather[0].icon}@2x.png`)
+                        j+=3
+                        z++
+                    }
                 })
         })
 }
